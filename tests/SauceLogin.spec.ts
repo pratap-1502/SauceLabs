54d5ecb53d2cfa59ad73763ef9df19c84647b040
Pratap <dvpratap40@email.com>
2026-08-11 05:16:30 +0530

    updated for tags

--- tests/SauceLogin.spec.ts
+++ tests/SauceLogin.spec.ts
@@ -10,33 +10,32 @@ test.beforeEach(async ({ page }) => {
 
 });
 
-test("Check Products label", async ({ page }) => {
+test("Check Products label", { tag: "@smoke" }, async ({ page }) => {
 
     await expect(page.getByText("Products")).toBeVisible();
 
 });
 
-test("Check Sauce Labs Backpack label", async ({ page }) => {
+test("Check shopping cart", { tag: "@smoke" }, async ({ page }) => {
+
+    await expect(page.locator(".shopping_cart_link")).toBeVisible();
+
+});
+
+test("Check Sauce Labs Backpack label", { tag: "@sanity" }, async ({ page }) => {
 
     await expect(page.getByText("Sauce Labs Backpack")).toBeVisible();
 
 });
 
-test("Check Sauce Labs Bike Light label", async ({ page }) => {
+test("Check Sauce Labs Bike Light label", { tag: "@sanity" }, async ({ page }) => {
 
     await expect(page.getByText("Sauce Labs Bike Light")).toBeVisible();
 
 });
 
-
-test("Check Sauce Labs Fleece Jacket label", async ({ page }) => {
+test("Check Sauce Labs Fleece Jacket label", { tag: "@regression" }, async ({ page }) => {
 
     await expect(page.getByText("Sauce Labs Fleece Jacket")).toBeVisible();
-
-});
-
-test("Check shopping cart", async ({ page }) => {
-
-    await expect(page.locator(".shopping_cart_link")).toBeVisible();
 
 });
